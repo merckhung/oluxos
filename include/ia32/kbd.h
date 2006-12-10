c821ec4142cf9c485d865b1f2671e24ea8549ff3
merck <merck@065ec012-7ff9-0310-94e7-8ecce7edae70>
2006-12-10 18:30:11 +0000

    Forget add kdb.h
    git-svn-id: file:///home/web/subversion/Develop/OluxOS/trunk@82 065ec012-7ff9-0310-94e7-8ecce7edae70

---
+++ include/ia32/kbd.h
@@ -0,0 +1,21 @@
+/*
+ * Copyright (C) 2007 Olux Organization All rights reserved.
+ * Author: Merck Hung <merck@olux.org>
+ *
+ * @OLUXORG_LICENSE_HEADER_START@
+ * @OLUXORG_LICENSE_HEADER_END@
+ *
+ */
+
+
+#define     KBCNT_PORT      0x60
+#define     KB8042_PORT     0x64
+
+
+void ia32_KbInitKeyboard( void );
+void ia32_KbIntHandler( void );
+
+void ia32_Kb8042SendCmd( __u8 cmd );
+void ia32_KbSendCmd( __u8 cmd );
+
+
