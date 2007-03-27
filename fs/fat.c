eac979d7ebfc872aec4e45a7a0b6d8e3833fc991
merck <merck@065ec012-7ff9-0310-94e7-8ecce7edae70>
2007-03-27 19:21:07 +0000

    Added basic FAT BPB structure and dump
    git-svn-id: file:///home/web/subversion/Develop/OluxOS/trunk@134 065ec012-7ff9-0310-94e7-8ecce7edae70

--- fs/fat.c
+++ fs/fat.c
@@ -19,11 +19,13 @@ void FsFatInit( void ) {
 
     __s8 buf[ 512 ];
     __s32 i;
+    FsFatBPB_t *p;
+
 
     ReadSector( 0 );
     ReadData( buf );
 
-    
+
     for( i = 0 ; i < 512 ; i++ ) {
 
         if( !( i % 26 ) ) {
@@ -33,6 +35,44 @@ void FsFatInit( void ) {
 
         DbgPrint( "%2X ", *(buf + i) );
     }
+
+    
+    p = (FsFatBPB_t *)buf;
+
+
+#if 1
+    DbgPrint( "\n" );
+    DbgPrint( "BS_OEMName     = %s\n", p->BS_OEMName );
+    DbgPrint( "BPB_BytePerSec = 0x%4X\n", p->BPB_BytePerSec );
+    DbgPrint( "BPB_SecPerClus = 0x%2X\n", p->BPB_SecPerClus );
+    DbgPrint( "BPB_RsvdSecCnt = 0x%4X\n", p->BPB_RsvdSecCnt );
+    DbgPrint( "BPB_NumFATs    = 0x%2X\n", p->BPB_NumFATs );
+    DbgPrint( "BPB_RootEntCnt = 0x%4X\n", p->BPB_RootEntCnt );
+    DbgPrint( "BPB_TotSec16   = 0x%4X\n", p->BPB_TotSec16 );
+    DbgPrint( "BPB_Media      = 0x%2X\n", p->BPB_Media );
+    DbgPrint( "BPB_FATSz16    = 0x%4X\n", p->BPB_FATSz16 );
+    DbgPrint( "BPB_SecPerTrk  = 0x%4X\n", p->BPB_SecPerTrk );
+    DbgPrint( "BPB_NumHeads   = 0x%4X\n", p->BPB_NumHeads );
+    DbgPrint( "BPB_HiddSec    = 0x%8X\n", p->BPB_HiddSec );
+    DbgPrint( "BPB_TotSec32   = 0x%8X\n", p->BPB_TotSec32 );
+
+
+    if( p->BPB_FATSz16 ) {
+    
+        FsFatBPBFat16_t *p16 = p + 37;
+    
+        DbgPrint( "BS_DrvNum    = 0x%2X\n", p16->BS_DrvNum );
+        DbgPrint( "BS_Reserved1 = 0x%2X\n", p16->BS_Reserved1 );
+        DbgPrint( "BS_BootSig   = 0x%2X\n", p16->BS_BootSig );
+
+        if( p16->BS_BootSig == 0x29 ) {
+
+            DbgPrint( "BS_VolID      = 0x%8X\n", p16->BS_VolID );
+            DbgPrint( "BS_VolLab     = 0x%s\n", p16->BS_VolLab );
+            DbgPrint( "BS_FilSysType = 0x%s\n", p16->BS_FilSysType );
+        }
+    }
+#endif
 }
 
 
--- include/fs/fat.h
+++ include/fs/fat.h
@@ -8,6 +8,57 @@
  */
 
 
+typedef struct {
+
+    __s8    BS_jmpBoot[ 3 ];
+    __s8    BS_OEMName[ 8 ];
+    __u16   BPB_BytePerSec;
+    __u8    BPB_SecPerClus;
+    __u16   BPB_RsvdSecCnt;
+    __u8    BPB_NumFATs;
+    __u16   BPB_RootEntCnt;
+    __u16   BPB_TotSec16;
+    __u8    BPB_Media;
+    __u16   BPB_FATSz16;
+    __u16   BPB_SecPerTrk;
+    __u16   BPB_NumHeads;
+    __u32   BPB_HiddSec;
+    __u32   BPB_TotSec32;
+
+} FsFatBPB_t;
+
+
+typedef struct {
+
+    __u8    BS_DrvNum;
+    __u8    BS_Reserved1;
+    __u8    BS_BootSig;
+    __u32   BS_VolID;
+    __s8    BS_VolLab[ 11 ];
+    __s8    BS_FilSysType[ 8 ];
+
+} FsFatBPBFat16_t;
+
+
+typedef struct {
+
+    __u32   BPB_FATSz32;
+    __u16   BPB_ExtFlags;
+    __u16   BPB_FSVer;
+    __u32   BPB_RootClus;
+    __u16   BPB_FSInfo;
+    __u16   BPB_BkBootSec;
+    __s8    BPB_Reserved[ 12 ];
+    __u8    BS_DrvNum;
+    __u8    BS_Reserved1;
+    __u8    BS_BootSig;
+    __u32   BS_VolID;
+    __s8    BS_VolLab[ 11 ];
+    __s8    BS_FilSysType[ 8 ];
+
+} FsFatBPBFat32_t;
+
+
 void FsFatInit( void );
 
 
