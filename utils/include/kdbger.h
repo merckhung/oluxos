052f9c4279c2f935d497a597dde759912d971c42
Merck Hung <merckhung@gmail.com>
2011-02-08 14:51:40 +0800

    1) Preliminary done HW function select code. 2) Preliminary implement cursor moving.

--- utils/include/kdbger.h
+++ utils/include/kdbger.h
@@ -2,4 +2,206 @@
 #define KDBGER_MAX_PATH				40
 #define KDBGER_DEF_TTYDEV			"/dev/pts/0"
 
+#define KDBGER_MIN_LINE				0
+#define KDBGER_MIN_COLUMN			0
+#define KDBGER_MAX_LINE				23
+#define KDBGER_MAX_COLUMN           80
+
+#define KDBGER_STRING_NLINE			1
+#define KDBGER_MAX_TIMESTR			8
+#define KDBGER_BYTE_PER_SCREEN		256
+
+#define KDBGER_PROGRAM_FNAME		"OluxOS Kernel Debugger"
+#define KDBGER_AUTHER_NAME			"Merck Hung <merckhung@gmail.com>"
+
+#define KDBGER_INFO_BAR_FMT			"Type: "
+
+#define KDBGER_INFO_MEMORY_FUNC		"Memory"
+#define KDBGER_INFO_IO_FUNC			"I/O"
+#define KDBGER_INFO_PCI_FUNC		"PCI/PCI-E"
+#define KDBGER_INFO_PCIL_FUNC		"PCI/PCI-E Listing"
+
+#define KDBGER_INFO_MEMORY_BASE		KDBGER_INFO_BAR_FMT KDBGER_INFO_MEMORY_FUNC "\tAddress: "
+#define KDBGER_INFO_IO_BASE			KDBGER_INFO_BAR_FMT KDBGER_INFO_IO_FUNC "\tAddress: "
+#define KDBGER_INFO_PCI_BASE		KDBGER_INFO_BAR_FMT KDBGER_INFO_PCI_FUNC "\t "
+
+#define KDBGER_INFO_MEMORY_BASE_FMT	"%8.8X-%8.8Xh"
+#define KDBGER_INFO_IO_BASE_FMT		"%4.4Xh"
+#define KDBGER_INFO_PCI_BASE_FMT	"Bus: %2.2Xh, Dev: %2.2Xh, Fun: %2.2Xh"
+
+#define KDBGER_INFO_LINE			(KDBGER_MAX_LINE - 1)
+#define KDBGER_INFO_COLUMN			0
+
+#define KDBGER_DUMP_TOP_BAR			"0000 00 01 02 03 04 05 06 07 08 09 0A 0B 0C 0D 0E 0F"
+#define KDBGER_DUMP_LEFT_BAR		"000000100020003000400050006000700080009000A000B000C000D000E000F0"
+#define KDBGER_DUMP_RTOP_BAR		"0123456789ABCDEF"
+
+#define KDBGER_DUMP_TOP_LINE		4
+#define KDBGER_DUMP_TOP_COLUMN		1
+
+#define KDBGER_DUMP_LEFT_LINE		(KDBGER_DUMP_TOP_LINE + 1)
+#define KDBGER_DUMP_LEFT_COLUMN		1
+
+#define KDBGER_DUMP_RTOP_LINE		KDBGER_DUMP_TOP_LINE
+#define KDBGER_DUMP_RTOP_COLUMN		(KDBGER_DUMP_TOP_COLUMN + strlen(KDBGER_DUMP_TOP_BAR) + 2)
+
+#define KDBGER_DUMP_BYTE_PER_LINE	16
+#define KDBGER_DUMP_BUF_PER_LINE	(KDBGER_DUMP_BYTE_PER_LINE * 3 - 1)
+
+#define KDBGER_DUMP_VALUE_LINE		(KDBGER_DUMP_TOP_LINE + 1)
+#define KDBGER_DUMP_VALUE_COLUMN	(KDBGER_DUMP_LEFT_COLUMN + 5)
+#define KDBGER_DUMP_ASCII_LINE		(KDBGER_DUMP_TOP_LINE + 1)
+#define KDBGER_DUMP_ASCII_COLUMN	KDBGER_DUMP_RTOP_COLUMN
+#define KDBGER_DUMP_ASCII_FILTER(x)	(((x) >= '!' && (x) <= '~')?(x):'.')
+
+#define KDBGER_DUMP_VBUF_SZ			(KDBGER_BYTE_PER_SCREEN * 3 - KDBGER_DUMP_BYTE_PER_LINE)
+#define KDBGER_DUMP_ABUF_SZ			KDBGER_BYTE_PER_SCREEN
+
+#define KDBGER_STS_BUF_SZ			4096
+#define KDBGER_STS_INTV_SECS		1
+
+
+#define printWindow( RESRC, NAME, LINE, COLUMN, X, Y, COLORPAIR, FORMAT, ARGS... ) {\
+    RESRC->NAME = newwin( LINE, COLUMN, X, Y );\
+    RESRC->panel##NAME = new_panel( RESRC->NAME );\
+    wbkgd( RESRC->NAME, COLOR_PAIR( COLORPAIR ) );\
+    wattrset( RESRC->NAME, COLOR_PAIR( COLORPAIR ) | A_BOLD );\
+    wprintw( RESRC->NAME, FORMAT, ##ARGS );\
+    wattrset( RESRC->NAME, A_NORMAL );\
+}
+
+
+#define printWindowAt( RESRC, NAME, LINE, COLUMN, X, Y, COLORPAIR, FORMAT, ARGS... ) {\
+    if( !RESRC->NAME ) {\
+        RESRC->NAME = newwin( LINE, COLUMN, X, Y );\
+        RESRC->panel##NAME = new_panel( RESRC->NAME );\
+    }\
+    wbkgd( RESRC->NAME, COLOR_PAIR( COLORPAIR ) );\
+    wattrset( RESRC->NAME, COLOR_PAIR( COLORPAIR ) | A_BOLD );\
+    mvwprintw( RESRC->NAME, 0, 0, FORMAT, ##ARGS );\
+    wattrset( RESRC->NAME, A_NORMAL );\
+}
+
+
+#define printWindowMove( RESRC, NAME, LINE, COLUMN, X, Y, COLORPAIR, FORMAT, ARGS... ) {\
+    if( RESRC->panel##NAME ) {\
+        del_panel( RESRC->panel##NAME );\
+        RESRC->panel##NAME = NULL;\
+    }\
+    if( RESRC->NAME ) {\
+        delwin( RESRC->NAME );\
+        RESRC->NAME = NULL;\
+    }\
+    RESRC->NAME = newwin( LINE, COLUMN, X, Y );\
+    RESRC->panel##NAME = new_panel( RESRC->NAME );\
+    wbkgd( RESRC->NAME, COLOR_PAIR( COLORPAIR ) );\
+    wattrset( RESRC->NAME, COLOR_PAIR( COLORPAIR ) | A_BOLD );\
+    mvwprintw( RESRC->NAME, 0, 0, FORMAT, ##ARGS );\
+    wattrset( RESRC->NAME, A_NORMAL );\
+}
+
+
+#define destroyWindow( RESRC, NAME ) {\
+    if( RESRC->panel##NAME ) {\
+        del_panel( RESRC->panel##NAME );\
+        RESRC->panel##NAME = NULL;\
+    }\
+    if( RESRC->NAME ) {\
+        delwin( RESRC->NAME );\
+        RESRC->NAME = NULL;\
+    }\
+}
+
+
+typedef enum {
+
+	KBPRS_ESC = 0x1B,
+	KBPRS_UP = 0x103,
+	KBPRS_DOWN = 0x102,
+	KBPRS_LEFT = 0x104,
+	KBPRS_RIGHT = 0x105,
+	KBPRS_PGUP = 0x153,
+	KBPRS_PGDN = 0x152,
+	KBPRS_F1 = 0x109,
+
+	KBPRS_U_M = 'M',
+	KBPRS_L_M = 'm',
+
+	KBPRS_U_I = 'I',
+	KBPRS_L_I = 'i',
+
+	KBPRS_U_P = 'P',
+	KBPRS_L_P = 'p',
+
+	KBPRS_U_L = 'L',
+	KBPRS_L_L = 'l',
+
+} kdbgerKeyPress_t;
+
+
+typedef enum {
+
+	KHF_MEM,
+	KHF_IO,
+	KHF_PCI,
+	KHF_PCIL,
+
+} kdbgerHwFunc_t;
+
+
+typedef enum {
+
+    WHITE_RED = 1,
+    WHITE_BLUE,
+    BLACK_WHITE,
+    CYAN_BLUE,
+    RED_BLUE,
+    YELLOW_BLUE,
+    BLACK_GREEN,
+    BLACK_YELLOW,
+    YELLOW_RED,
+    YELLOW_BLACK,
+    WHITE_YELLOW,
+	RED_WHITE,
+
+} kdbgerColorPairs_t;
+
+
+typedef struct {
+
+    PANEL   *panelbackground;
+    PANEL   *panellogo;
+    PANEL   *panelcopyright;
+    PANEL   *panelstatus;
+    PANEL   *paneltime;
+
+    WINDOW  *background;
+    WINDOW  *logo;
+    WINDOW  *copyright;
+    WINDOW  *status;
+    WINDOW  *time;
+
+} kdbgerBasePanel_t;
+
+
+typedef struct {
+
+	PANEL *paneltop;
+	PANEL *panelleft;
+	PANEL *panelrtop;
+    PANEL *panelvalue;
+    PANEL *panelascii;
+	PANEL *panelinfo;
+	PANEL *panelhighlight;
+
+	WINDOW *top;
+	WINDOW *left;
+	WINDOW *rtop;
+    WINDOW *value;
+    WINDOW *ascii;
+	WINDOW *info;
+	WINDOW *highlight;
+
+} kdbgerDumpPanel_t, kdbgerMemoryPanel_t;
+
 
--- utils/kdbger.c
+++ utils/kdbger.c
@@ -19,8 +19,12 @@
 #include <termios.h>
 
 #include <otypes.h>
-#include <kdbger.h>
 #include <packet.h>
+
+#include <ncurses.h>
+#include <panel.h>
+
+#include <kdbger.h>
 
 
 static void help( void ) {
@@ -37,13 +41,12 @@ static void help( void ) {
 
 
 static void debugPrintBuffer( s8 *pBuf, u32 size, u32 base ) {
-#define LINE_DIGITS	16
 
     u32 i, j;
-	u8 buf[ LINE_DIGITS ];
+	u8 buf[ KDBGER_DUMP_BYTE_PER_LINE ];
 	s8 unalign = 0;
 
-	if( size % LINE_DIGITS ) {
+	if( size % KDBGER_DUMP_BYTE_PER_LINE ) {
 
 		unalign = 1;
 	}
@@ -53,10 +56,10 @@ static void debugPrintBuffer( s8 *pBuf, u32 size, u32 base ) {
 
     for( i = 0 ; i <= size ; i++ ) {
 
-        if( !(i % LINE_DIGITS) ) {
+        if( !(i % KDBGER_DUMP_BYTE_PER_LINE) ) {
 
 			if( i ) {
-				for( j = 0 ; j < LINE_DIGITS ; j++ ) {
+				for( j = 0 ; j < KDBGER_DUMP_BYTE_PER_LINE ; j++ ) {
 
 					if( buf[ j ] >= '!' && buf[ j ] <= '~' )
 						printf( "%c", buf[ j ] );
@@ -73,16 +76,16 @@ static void debugPrintBuffer( s8 *pBuf, u32 size, u32 base ) {
 			memset( buf, ' ', sizeof( buf ) );
         }
 
-		buf[ i % LINE_DIGITS ] = (u8)(*(pBuf + i));
-        printf( "%2.2X ", buf[ i % LINE_DIGITS ] & 0xFF );
+		buf[ i % KDBGER_DUMP_BYTE_PER_LINE ] = (u8)(*(pBuf + i));
+        printf( "%2.2X ", buf[ i % KDBGER_DUMP_BYTE_PER_LINE ] & 0xFF );
     }
 
 	if( unalign ) {
 
-		for( j = LINE_DIGITS - (size % LINE_DIGITS) - 1 ; j-- ; ) 
+		for( j = KDBGER_DUMP_BYTE_PER_LINE - (size % KDBGER_DUMP_BYTE_PER_LINE) - 1 ; j-- ; ) 
 			printf( "   " );
 
-		for( j = 0 ; j < (size % LINE_DIGITS) ; j++ )
+		for( j = 0 ; j < (size % KDBGER_DUMP_BYTE_PER_LINE) ; j++ )
 			if( buf[ j ] >= '!' && buf[ j ] <= '~' )
 				printf( "%c", buf[ j ] );
 			else
@@ -272,6 +275,274 @@ s32 executeFunction( s32 fd, kdbgerOpCode_t op, u64 addr, u32 size, s8 *cntBuf, 
 }
 
 
+void initColorPairs( void ) {
+
+    init_pair( WHITE_RED, COLOR_WHITE, COLOR_RED );
+    init_pair( WHITE_BLUE, COLOR_WHITE, COLOR_BLUE );
+    init_pair( BLACK_WHITE, COLOR_BLACK, COLOR_WHITE );
+    init_pair( CYAN_BLUE, COLOR_CYAN, COLOR_BLUE );
+    init_pair( RED_BLUE, COLOR_RED, COLOR_BLUE );
+    init_pair( YELLOW_BLUE, COLOR_YELLOW, COLOR_BLUE );
+    init_pair( BLACK_GREEN, COLOR_BLACK, COLOR_GREEN );
+    init_pair( BLACK_YELLOW, COLOR_BLACK, COLOR_YELLOW );
+    init_pair( YELLOW_RED, COLOR_YELLOW, COLOR_RED );
+    init_pair( YELLOW_BLACK, COLOR_YELLOW, COLOR_BLACK );
+    init_pair( WHITE_YELLOW, COLOR_WHITE, COLOR_YELLOW );
+	init_pair( RED_WHITE, COLOR_RED, COLOR_WHITE );
+}
+
+
+void printBasePlane( kdbgerBasePanel_t *pKdbgerBasePanel ) {
+
+    // Background Color
+    printWindow( pKdbgerBasePanel, background, KDBGER_MAX_LINE, KDBGER_MAX_COLUMN, KDBGER_MIN_LINE, KDBGER_MIN_COLUMN, WHITE_BLUE, "" );
+
+    // Logo
+    printWindow( pKdbgerBasePanel, logo, KDBGER_STRING_NLINE, KDBGER_MAX_COLUMN, KDBGER_MIN_LINE, KDBGER_MIN_COLUMN, WHITE_RED, KDBGER_PROGRAM_FNAME );
+
+	// Copyright
+    printWindow( pKdbgerBasePanel, copyright, KDBGER_STRING_NLINE, strlen( KDBGER_AUTHER_NAME ), KDBGER_MIN_LINE, KDBGER_MAX_COLUMN - strlen( KDBGER_AUTHER_NAME ), WHITE_RED, KDBGER_AUTHER_NAME );
+
+	// Update screen
+    update_panels();
+    doupdate();
+}
+
+
+void updateStatusTimer( kdbgerBasePanel_t *pKdbgerBasePanel, u32 *lastSecond, s8 *strPtr, s32 *strIdx ) {
+
+	time_t timer;
+	struct tm *nowtime;
+	u32 thisSecond;
+	u8 update = 0;
+	s8 stsBuf[ KDBGER_MAX_COLUMN - KDBGER_MAX_TIMESTR ];
+
+	// Read second
+	thisSecond = time( &timer );
+	if( thisSecond == -1 )
+		return;
+
+	// Convert to localtime
+	nowtime = localtime( &timer );
+	if( !nowtime )
+		return;
+
+	if( (*lastSecond) < thisSecond ) {
+
+		// Update timer per second
+		printWindowAt( pKdbgerBasePanel, 
+			time,
+			KDBGER_STRING_NLINE,
+			KDBGER_MAX_TIMESTR,
+			KDBGER_MAX_LINE,
+			KDBGER_MAX_COLUMN - KDBGER_MAX_TIMESTR,
+			BLACK_WHITE,
+			"%2.2d:%2.2d:%2.2d",
+			nowtime->tm_hour,
+			nowtime->tm_min,
+			nowtime->tm_sec );
+
+		update = 1;
+	}
+
+	if( strPtr && ((thisSecond - (*lastSecond)) >= KDBGER_STS_INTV_SECS) ) {
+
+		s32 len = strlen( strPtr );
+		if( *strIdx >= len )
+			*strIdx = 0;
+
+		if( (len - *strIdx) > (KDBGER_MAX_COLUMN - KDBGER_MAX_TIMESTR) )
+			len = KDBGER_MAX_COLUMN - KDBGER_MAX_TIMESTR;
+
+		snprintf( stsBuf, len, "%s", (strPtr + *strIdx) );
+
+		// Update status bar
+    	printWindow( pKdbgerBasePanel, status, KDBGER_STRING_NLINE, KDBGER_MAX_COLUMN - KDBGER_MAX_TIMESTR, KDBGER_MAX_LINE, KDBGER_MIN_COLUMN, RED_WHITE, "%s", stsBuf );
+
+		(*strIdx)++;
+		update = 1;
+	}
+
+	if( update )
+		*lastSecond = thisSecond;
+}
+
+
+void printDumpBasePanel( kdbgerDumpPanel_t *pKdbgerDumpPanel, s8 *infoStr ) {
+
+	// Print Top bar
+	printWindowAt( pKdbgerDumpPanel,
+		top, 
+		KDBGER_STRING_NLINE,
+		strlen( KDBGER_DUMP_TOP_BAR ),
+		KDBGER_DUMP_TOP_LINE,
+		KDBGER_DUMP_TOP_COLUMN,
+		RED_BLUE,
+		KDBGER_DUMP_TOP_BAR );
+
+	// Print Rtop bar
+	printWindowAt( pKdbgerDumpPanel,
+		rtop, 
+		KDBGER_STRING_NLINE,
+		KDBGER_DUMP_BYTE_PER_LINE,
+		KDBGER_DUMP_RTOP_LINE,
+		KDBGER_DUMP_RTOP_COLUMN,
+		RED_BLUE,
+		KDBGER_DUMP_RTOP_BAR );
+
+	// Print Left bar
+	printWindowAt( pKdbgerDumpPanel,
+		left,
+		KDBGER_DUMP_BYTE_PER_LINE,
+		4,
+		KDBGER_DUMP_LEFT_LINE,
+		KDBGER_DUMP_LEFT_COLUMN,
+		RED_BLUE,
+		KDBGER_DUMP_LEFT_BAR );
+
+	// Print Info base
+	printWindowAt( pKdbgerDumpPanel,
+		info,
+		KDBGER_STRING_NLINE,
+		KDBGER_MAX_COLUMN,
+		KDBGER_INFO_LINE,
+		KDBGER_INFO_COLUMN,
+		WHITE_BLUE,
+		"%s",
+		infoStr );
+}
+
+
+void printDumpUpdatePanel( kdbgerDumpPanel_t *pKdbgerDumpPanel, u8 *dataPtr, s32 *offset ) {
+
+	s32 i, x, y;
+	u8 valueBuf[ KDBGER_DUMP_VBUF_SZ + 1 ];
+	u8 asciiBuf[ KDBGER_DUMP_ABUF_SZ + 1 ];
+	u8 *vp = valueBuf, *ap = asciiBuf;
+	u8 *pDataPtr = dataPtr;
+
+	// Terminate buffers
+	valueBuf[ KDBGER_DUMP_VBUF_SZ ] = 0;
+	asciiBuf[ KDBGER_DUMP_ABUF_SZ ] = 0;
+
+	// Format data for value & ascii
+	for( i = 0 ; i < KDBGER_DUMP_BYTE_PER_LINE ; i++ ) {
+		 
+		vp += sprintf( (s8 *)vp,
+		"%2.2X %2.2X %2.2X %2.2X %2.2X %2.2X %2.2X %2.2X %2.2X %2.2X %2.2X %2.2X %2.2X %2.2X %2.2X %2.2X",
+		dataPtr[ 0 ], dataPtr[ 1 ], dataPtr[ 2 ], dataPtr[ 3 ], dataPtr[ 4 ], dataPtr[ 5 ], dataPtr[ 6 ],
+		dataPtr[ 7 ], dataPtr[ 8 ], dataPtr[ 9 ], dataPtr[ 10 ], dataPtr[ 11 ], dataPtr[ 12 ], dataPtr[ 13 ],
+		dataPtr[ 14 ], dataPtr[ 15 ] );
+
+		ap += sprintf( (s8 *)ap,
+		"%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c",
+		KDBGER_DUMP_ASCII_FILTER( dataPtr[ 0 ] ),
+		KDBGER_DUMP_ASCII_FILTER( dataPtr[ 1 ] ),
+        KDBGER_DUMP_ASCII_FILTER( dataPtr[ 2 ] ),
+        KDBGER_DUMP_ASCII_FILTER( dataPtr[ 3 ] ),
+        KDBGER_DUMP_ASCII_FILTER( dataPtr[ 4 ] ),
+        KDBGER_DUMP_ASCII_FILTER( dataPtr[ 5 ] ),
+        KDBGER_DUMP_ASCII_FILTER( dataPtr[ 6 ] ),
+        KDBGER_DUMP_ASCII_FILTER( dataPtr[ 7 ] ),
+        KDBGER_DUMP_ASCII_FILTER( dataPtr[ 8 ] ),
+        KDBGER_DUMP_ASCII_FILTER( dataPtr[ 9 ] ),
+        KDBGER_DUMP_ASCII_FILTER( dataPtr[ 10 ] ),
+        KDBGER_DUMP_ASCII_FILTER( dataPtr[ 11 ] ),
+        KDBGER_DUMP_ASCII_FILTER( dataPtr[ 12 ] ),
+        KDBGER_DUMP_ASCII_FILTER( dataPtr[ 13 ] ),
+        KDBGER_DUMP_ASCII_FILTER( dataPtr[ 14 ] ),
+        KDBGER_DUMP_ASCII_FILTER( dataPtr[ 15 ] ) );
+
+		// Move to next line
+		dataPtr += KDBGER_DUMP_BYTE_PER_LINE;
+	}
+
+	// Print value
+	printWindowAt( pKdbgerDumpPanel,
+		value, 
+		KDBGER_DUMP_BYTE_PER_LINE,
+		KDBGER_DUMP_BUF_PER_LINE,
+		KDBGER_DUMP_VALUE_LINE,
+		KDBGER_DUMP_VALUE_COLUMN,
+		WHITE_BLUE,
+		"%s",
+		valueBuf );
+
+	// Print ASCII
+	printWindowAt( pKdbgerDumpPanel,
+		ascii,
+		KDBGER_DUMP_BYTE_PER_LINE,
+		KDBGER_DUMP_BYTE_PER_LINE,
+		KDBGER_DUMP_ASCII_LINE,
+		KDBGER_DUMP_ASCII_COLUMN,
+		WHITE_BLUE,
+		"%s",
+		asciiBuf );
+
+	// Highlight
+	y = ((*offset) / KDBGER_DUMP_BYTE_PER_LINE) + KDBGER_DUMP_VALUE_LINE;
+	x = (((*offset) % KDBGER_DUMP_BYTE_PER_LINE) * 3) + KDBGER_DUMP_VALUE_COLUMN;
+	printWindowMove( pKdbgerDumpPanel,
+		highlight,
+		1,
+		2,
+		y,
+		x,
+		YELLOW_BLUE,
+		"%2.2X",
+		*(pDataPtr + (*offset)) );
+}
+
+
+void handleKeyPressForDumpPanel( kdbgerKeyPress_t kdbgerKeyPress, s32 *offset ) {
+
+	switch( kdbgerKeyPress ) {
+
+		case KBPRS_UP:
+
+			if( ((*offset) - KDBGER_DUMP_BYTE_PER_LINE) >= 0 )
+				(*offset) -= KDBGER_DUMP_BYTE_PER_LINE;
+			else
+				(*offset) = KDBGER_BYTE_PER_SCREEN - (KDBGER_DUMP_BYTE_PER_LINE - (*offset));
+            break;
+
+		case KBPRS_DOWN:
+
+			if( ((*offset) + KDBGER_DUMP_BYTE_PER_LINE) < KDBGER_BYTE_PER_SCREEN )
+				(*offset) += KDBGER_DUMP_BYTE_PER_LINE;
+			else
+				(*offset) %= KDBGER_DUMP_BYTE_PER_LINE;
+            break;
+
+		case KBPRS_LEFT:
+
+			if( ((*offset) - 1) >= 0 && ((*offset) % KDBGER_DUMP_BYTE_PER_LINE) > 0 )
+				(*offset)--;
+			else
+				(*offset) += (KDBGER_DUMP_BYTE_PER_LINE - 1);
+            break;
+
+		case KBPRS_RIGHT:
+
+			if( ((*offset) + 1) < KDBGER_BYTE_PER_SCREEN 
+				&& (((*offset) + 1) % KDBGER_DUMP_BYTE_PER_LINE) )
+				(*offset)++;
+			else
+				(*offset) -= (KDBGER_DUMP_BYTE_PER_LINE - 1);
+            break;
+
+		case KBPRS_PGUP:
+            break;
+
+		case KBPRS_PGDN:
+            break;
+
+		default:
+			break;
+	}
+}
+
+
 s32 main( s32 argc, s8 **argv ) {
 
 	s32 fd;
@@ -280,10 +551,19 @@ s32 main( s32 argc, s8 **argv ) {
 	s8 pktBuf[ KDBGER_MAXSZ_PKT ];
 	s8 cntBuf[ KDBGER_MAXSZ_PKT ];
 	kdbgerCommPkt_t *pKdbgerCommPkt = (kdbgerCommPkt_t *)pktBuf;
+	kdbgerBasePanel_t kdbgerBasePanel;
+	kdbgerMemoryPanel_t kdbgerMemoryPanel;
+	s32 inputBuf, dumpByteOffset = 0, strIdx = 0;
+	u32 lastSecond = 0;
+	kdbgerHwFunc_t kdbgerHwFunc = KHF_MEM;
 
+
+	s8 statusBarString[ KDBGER_STS_BUF_SZ ] = "(m)Memory (i)I/O (p)PCI/PCI-E (l)PCI/PCI-E List (c)CMOS (h)Help";
 
 	// Initialization
 	strncpy( ttyDevice, KDBGER_DEF_TTYDEV, KDBGER_MAX_PATH );
+	memset( &kdbgerBasePanel, 0, sizeof( kdbgerBasePanel_t ) );
+	memset( &kdbgerMemoryPanel, 0, sizeof( kdbgerMemoryPanel_t ) );
 
 
 	// Handle parameters
@@ -326,6 +606,111 @@ s32 main( s32 argc, s8 **argv ) {
 	}
 
 
+	// Initialize ncurses
+    initscr();
+    start_color();
+    cbreak();
+    noecho();
+    nodelay( stdscr, 1 );
+    keypad( stdscr, 1 );
+    curs_set( 0 );
+	initColorPairs();
+
+
+	// Print base panel
+	printBasePlane( &kdbgerBasePanel );
+
+
+	// Read memory
+	executeFunction( fd, KDBGER_REQ_MEM_READ, 0x7C00, KDBGER_BYTE_PER_SCREEN, NULL, pktBuf, KDBGER_MAXSZ_PKT );
+
+
+	printDumpBasePanel( &kdbgerMemoryPanel, KDBGER_INFO_MEMORY_BASE );
+
+
+	// Main loop
+	for( ; ; ) {
+
+		// Get keyboard input
+		inputBuf = getch();
+/*
+		if( inputBuf >= 0 )
+			printf( "0x%8.8X\n", inputBuf );
+*/
+		switch( inputBuf ) {
+
+			// ESC
+			case KBPRS_ESC:
+				goto Exit;
+
+			// Help
+			case KBPRS_F1:
+				break;
+
+			// PCI/PCI-E listing
+			case KBPRS_U_L:
+			case KBPRS_L_L:
+				kdbgerHwFunc = KHF_PCIL;
+				break;
+
+			// PCI/PCI-E config space
+			case KBPRS_U_P:
+			case KBPRS_L_P:
+				kdbgerHwFunc = KHF_PCI;
+				break;
+
+			// I/O
+			case KBPRS_U_I:
+			case KBPRS_L_I:
+				kdbgerHwFunc = KHF_IO;
+				break;
+
+			// Memory
+			default:
+			case KBPRS_U_M:
+			case KBPRS_L_M:
+				kdbgerHwFunc = KHF_MEM;
+				break;
+		}
+
+
+		switch( kdbgerHwFunc ) {
+
+			default:
+			case KHF_MEM:
+				handleKeyPressForDumpPanel( inputBuf, &dumpByteOffset );
+				printDumpUpdatePanel( &kdbgerMemoryPanel, (u8 *)&pKdbgerCommPkt->kdbgerRspMemReadPkt.memContent, &dumpByteOffset );
+				break;
+
+			case KHF_IO:
+				break;
+
+			case KHF_PCI:
+				break;
+
+			case KHF_PCIL:
+				break;
+		}
+
+
+        // Update timer
+		updateStatusTimer( &kdbgerBasePanel, &lastSecond, statusBarString, &strIdx );
+
+		// Refresh Screen
+		update_panels();
+		doupdate();
+
+		// Delay for a while
+		usleep( 1000 );
+	}
+
+Exit:
+
+	// Terminate ncurses
+	endwin();
+
+
+/*
 	// Read memory
 	if( executeFunction( fd, KDBGER_REQ_MEM_READ, 0x7c00, 256, NULL, pktBuf, KDBGER_MAXSZ_PKT ) ) {
 
@@ -363,6 +748,7 @@ s32 main( s32 argc, s8 **argv ) {
 	}
 	debugPrintBuffer( (s8 *)&pKdbgerCommPkt->kdbgerRspIoReadPkt.ioContent,
 		pKdbgerCommPkt->kdbgerRspIoReadPkt.size, (u32)pKdbgerCommPkt->kdbgerRspIoReadPkt.address );
+*/
 
 
 ErrExit:
